8df3d58667ecba0f36263793a7bc8d30526eea75
HIT <hiteshshikne@gmail.com>
2020-11-30 20:30:40 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home - LTCE</title>
+	<link rel="stylesheet" type="text/css" href="Style.css">
+</head>
+<body bgcolor="Black">
+  <div class="container"><!--container start-->
+  	<!--header start-->
+  	<div class="header">
+  	<img src="logo.png" width="500" height="100">
+    <div class="userarea"><!--userarea start-->
+      <form action="https://www.google.com/search" method="get">
+            <input type="text" name="q" class="search" placeholder="search Here">
+            <input type="submit" name="submit" class="submit" value="Search">
+       </form>
+    </div><!--userarea end-->
+  	</div>
+    <!--header end-->
+
+<div class="menucontainer">
+ <!--menucontainer start--> 
+	<nav class="menu">
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="login.php">Login</a></li>
+      <li><a href="register.php">Register</a></li>
+      
+      <!--sub link-->
+  </ul>
+</nav>
+</div>
+<!--menucontainer end-->
+
+
+<!--banner section start-->
+<div class="banner">
+	<div class="item">
+		<div class="hero">
+
+          <div class ="button">
+              <a href ="login.php" class ="btn btn-one">Login</a>
+              <a href ="register.php" class ="btn btn-two "> Register</a>
+          </div>
+     </div>
+	</div><!--item-->
+</div>
+<!--banner section end-->
+
+
+<!--About LTCE-->
+<div class="about">
+	<h2>About LTCE</h2>
+	<hr>
+	<div class="aboutus">
+    <p>   <b> Our Vision</b>
+    <br>
+To create technically competent and ethically responsible professionals capable of providing efficient solutions to the contemporary world.
+    </p><p><b> Our Mission</b>
+    <br>
+We aim to excel in our continual efforts, towards being one of the most recognized institutions, by:
+Providing a conducive environment comprising high-end infrastructure and state-of-the-art laboratory facilities wherein the students, faculty and staff can collectively enhance their technical potential.
+Encouraging innovation through research activities for the benefit of society.
+Developing competent professionals responsive to change in technology.
+</p>
+    <br>
+		<p>
+		<h4>Department List of LTCE</h4></p>
+		<br>
+    <h5>The Institute Highlights </h5>
+    <ul style="list-style-type:square">
+    <li>•	Accredited by National Assessment and Accreditation.</li>
+    <li>•	Council (NAAC) Bangalore.</li>
+    <li>•	Over 10,000 successful Alumni worldwide</li>
+    <li>•	Excellent placements and 100% internship assistance
+for AY 2019 20</li>
+    <li>•	Placement with highest package of Rs 12 Lakh P. A. and avg package 3.2 Lakh P. A.</li>
+    <li>•	Highly Qualified and Experienced Faculty</li>
+    <li>•	Excellent infrastructure and facilities</li>
+    <li>•	Toppers with 10 CGPA and rank holders in University of Mumbai</li>
+    <li>•	Scholarship / Awards for eligible students.</li>
+    <li>•	MoU signed with 15+ industry for projects/Consultancies / internships/ research work.</li>
+    </ul>
+    <h3>DTE Code : 3196</h3>
+
+    <h5>Bachelor of Engineering (B.E.)</h5>
+    <ul style="list-style-type:square">
+    <li>•	Computer Engineering	intake {120}</li>
+    <li>•	Computer Science and Engineering
+(Data Science) intake {60}</li>
+    
+    <li>•	Computer Science and Engineering
+(Artificial Intelligence and Machine Learning) intake {60}</li>
+    <li>•	Computer Science and Engineering
+(IoT and Cyber Security including Block Chain Tech) intake {60}</li>
+    <li>•	Electrical Engineering intake {60}</li>
+    <li>•	Electronics & Telecommunication Engineering intake {60}</li>
+    <li>•	Mechanical Engineering intake {120}</li>
+    </ul> 
+    <h5>Master of Engineering (M.E.)</h5>
+    <ul style="list-style-type:square">
+    <li>•	Computer Engineering	intake {18}</li>
+    <li>•	Mechanical Engineering	intake {18}</li>
+    </ul>
+	</div>
+	
+</div>
+<!--About LTCE end-->
+
+<!--map and address-->
+ <div class="footer">
+ 	<!--map-->
+  <div class="row">
+  <div class="col-4">
+      <h3 class="head">COLLEGE LOCATION</h3>
+      <div class="map">
+      	<img src="map.png" width="170" height="170">
+      </div>
+  </div><!--col-4-->
+
+  <div class="col-4">
+       <h3 class="head">COLLEGE ADDRESS</h3>
+       <div class="para">
+      	<p>Postal Code: 400709</p>
+        <p>022 2754 1005</p>
+        <p>principal.ltce@ltjss.net</p>
+        <p>Monday - Friday:</p>
+        <p> 9:00 AM - 5:00 PM</p>
+    </div>
+  </div><!--col-4-->
+
+</div><!--row-->
+
+
+  <div class="clearfix">  
+  </div>
+</div>
+ 
+ 
+<!--map and address end-->
+
+
+  <footer class="copyright text-center">
+    &copy;2020 |<a href="https://ltce.in/"> Lokmanya Tilak College of Engineering </a>| All Rights Reserved
+    
+  </footer>
+              
+  </div>
+  <!--container end-->
+
+</body>
+</html>
